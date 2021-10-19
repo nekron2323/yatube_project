bdd3ffe55d04e3ffb5111ad8a3b62fb9a616004d
GoncharovUV <goncharov.uv@gmail.com>
2021-10-19 18:56:33 +0300

    Добавлены html, сделаны миграции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,22 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 
 def index(request):
-    print(request)
-    return HttpResponse('<h1>Main page</h1>')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'<h1>Группы постов {slug}</h1>')
+    template = 'posts/group_list.html'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': slug,
+        'text': text,
+    }
+    return render(request, template, context)
