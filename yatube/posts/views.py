0f03d5aebbc1feccd144cbd11de9e9bafc253d46
GoncharovUV <goncharov.uv@gmail.com>
2021-10-20 10:20:55 +0300

    Перед ревью

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Group, Post
-# Create your views here.
 
 
 def index(request):
@@ -11,12 +10,11 @@ def index(request):
     return render(request, 'posts/index.html', context)
 
 
-def group_posts(request, slug):    
+def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    template = 'posts/group_list.html'
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]    
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
         'title': slug,
         'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
